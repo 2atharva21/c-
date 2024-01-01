f899c9cea3a0bae338840f785dc5ac5ba1e36b93
Atharva zare <120238220+2atharva21@users.noreply.github.com>
2024-01-01 15:08:39 +0530

    first commit

---
+++ Variables_and_Output _Input in C++/basic-of-c++.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main(){
+int a;// this varible that store a value of number in container of a is called variable .
+a= 1444;
+int b;
+b= 13;
+int c=a+b;
+cout<<c;
+
+}
