9366f34eef273de264169bd54af68c064fa5fa65
Atharva zare <120238220+2atharva21@users.noreply.github.com>
2024-02-26 14:55:34 +0530

    X%9%6&&^LqLeFoQujHJ90M?fH

--- 8_opps-in-c++/basic-of-object-and-class.cpp
+++ 8_opps-in-c++/basic-of-object-and-class.cpp
@@ -1,19 +1,32 @@
 #include <iostream>
 using namespace std;
-class examples{
-public :
-string name ;
-int id;
-int roll;54
+class room{
+public:
+int lenght ;
+int breath;
+int height;
+int calculate_area(){
+  return lenght *breath;
+}
+int calculate_volume (){
+  return lenght*breath*height;
+}
 };
-
 int main (){
-examples s1;
-s1.name="atharva zare";
-s1.id=2;
-s1.roll=23;
-cout<<"name :"<<s1.name<<endl;
-cout<<"id :"<<s1.id<<endl;
-cout<<"roll no :"<<s1.roll<<endl;
+room room1;
+room1.lenght =10;
+room1.breath =20;
+room1.height =20;
+cout<<"sum of all area = "<<room1.calculate_area()<<endl;
+cout<<"sum of all volume = "<<room1.calculate_volume()<<endl;
 
 }
+// In this program, we have used the Room class and its object room1 to calculate the area and volume of a room.
+
+// In main(), we assigned the values of length, breadth, and height with the code:
+// oom1.length = 42.5;
+// room1.breadth = 30.8;
+// room1.height = 19.2;
+// We then called the functions calculateArea() and calculateVolume() to perform the necessary calculations.
+
+// Note the use of the keyword public in the program. This means the members are public and can be accessed anywhere from the program.
