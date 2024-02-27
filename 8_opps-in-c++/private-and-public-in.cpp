ce8529d1e5e2cea766c51c465587f4402b94ec8c
Atharva zare <120238220+2atharva21@users.noreply.github.com>
2024-02-27 11:14:30 +0530

    ilarious.blood

--- 8_opps-in-c++/private-and-public-in.cpp
+++ 8_opps-in-c++/private-and-public-in.cpp
@@ -1,29 +1,45 @@
+// Using public and private in C++ Class
 #include <iostream>
-using namespace std ;
-class employee {
-private:
-int a,b,c;
+using namespace std;
+class room {
+    public:
+
+int lenght;
+int breath;
+int height;
 public :
-int d,e;
-void setData(int a1,int  b1,int c1);
-void getData(){
-    cout<<"the value of a is "<<a<<endl;
-    cout<<"the value of b is "<<b<<endl;
-    cout<<"the value of c is "<<c<<endl;
-    cout<<"the value of e is "<<d<<endl;
-    cout<<"the value of d is "<<e<<endl;
+ void initData(int len,int bre,int hei){
+lenght =len;
+breath=bre;
+height=hei;
+ }
+int calculation_area(){
+    return lenght*breath;
 }
+int calculation_volume(){
+    return lenght*breath*height;
+}
+     
+
 };
-void employee::setData(int a1 ,int b1 ,int c1){
-    a=a1;
-    b=b1;
-    c=c1;
-}
 int main (){
-employee atharva;
-atharva.d=5;
-atharva.e=6;
-atharva.setData(2,3,4);
-atharva.getData();
-return 0;
+room room1;
+
+    // pass the values of private variables as arguments
+        room1.initData(425, 308, 192);
+
+    cout << "Area of Room =  " << room1.calculation_area() << endl;
+    cout << "Volume of Room =  " << room1.calculation_volume() << endl;
+
+    return 0;
 }
+// area of Room =  1309
+// Volume of Room =  25132.8
+// The above example is nearly identical to the first example, except that the class variables are now private.
+
+// Since the variables are now private, we cannot access them directly from main(). Hence, using the following code would be invalid:
+
+// // invalid code
+// obj.length = 42.5;
+// obj.breadth = 30.8;
+// obj.height = 19.2;
